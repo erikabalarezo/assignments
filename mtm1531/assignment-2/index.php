566f28cfc97799a9a600156c2390e8f89e4c0407
erikabalarezo <erikabalarezo@yahoo.com>
2012-05-17 17:58:23 -0400

    Moved <form> to bottom of index.php. Added variables to the form

--- mtm1531/assignment-2/index.php
+++ mtm1531/assignment-2/index.php
@@ -1,58 +1,71 @@
 <?php
+$result = 0;
+$num1 = 0;
+$num2 = 0;
+$total = 0;
+$function = 'plus';
+//var_dump($_POST);
 
-var_dump($_POST);
+
+if (isset($_POST['function']) && isset($_POST['num1']) && isset($_POST['num2'])) {
+	$num1 = $_POST['num1'];
+	$num2 = $_POST['num2'];
+	$function = $_POST['function'];
+	
+	if(($num2 > 0) && ($num1 > 0)) {
+		switch ($_POST['function']){
+			case "minus":
+				if($num1 >= $num2) 
+					$result =  $num1 - $num2;
+				break;
+			case "multiply":
+				$result =  $num1 * $num2;
+				break;
+			case "division":
+				$result = $num1 / $num2;
+				break;
+			case "plus":
+			default:
+				$result =  $num1 + $num2;
+				break;
+		}
+	}
+}
+$total = $result * 1.13;
+
 ?>
 
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Money Calculator Form</title>
+	<meta charset="utf-8">
+	<title>Money Calculator Form</title>
+	<link href="css/general.css" rel="stylesheet">
 </head>
 
 <body>
-	<?php if($_SERVER['REQUEST_METHOD']== 'GET') : ?>
-	<form method="post" action="index.php">
-		<label for="number-one">Number 1</label>
-		<input id="number-one" name="number-one">
-        
-        <label for="number-two">Number 2</label>
-        <input id="number-two" name="number-two">
-        
-        <label for="function">Function</label>
-        <select id="function" name="function">
-        	<option value="plus">+</option>
-            <option value="minus">-</option>
-            <option value="multiply">*</option>
-            <option value="division">/</option>
-         </select>
-		<button type="submit">Calculate</button>
+	<h1>Money Calculator Form</h1>
+	<form class = "moneyform" method="post" action="index.php"
+	
+		<label for="num1">Number 1</label>
+		<input type="number" id="num1" name="num1">
+		<label for="num2">Number 2</label>
+		<input type="number" id="num2" name="num2">
+
+		<label for="function">Function</label>
+		<select id="function" name="function">
+			<option value="plus">+</option>
+			<option value="minus">-</option>
+			<option value="multiply">*</option>
+			<option value="division">/</option>
+		</select>
+		<button>Calculate</button>
+		
+		<strong>$<?php echo number_format($total,2); ?></strong>
+		
+		
 	</form>
-	<?php else : ?>
-    	<?php
-			switch ($_POST['rating']){
-				case plus:
-				?>
-                	<h1><?php echo $_POST['dino-name'] ?> suck!</h1>
-                <?php
-				break;
-				case minus:
-				?>
-                	<h1><?php echo $_POST['dino-name'] ?> suck!</h1>
-                <?php
-				break;
-				case multiply:
-				?>
-                	<h1.<?php echo $_POST['dino-name']; ?> are OK.</h1>
-                <?php
-				break;
-				case division:
-				?>
-                	<h1><?php echo $_POST['dino-name']; ?> are amaze-a-zing!
-                <?php
-				break;
-	}
-	?>
-    <?php endif; ?>
+
 </body>
+
 </html>
