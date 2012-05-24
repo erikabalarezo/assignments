6cc91fbac18eb5d96dcbdccf4b5f1742a5be7dea
unknown <owner@owner-HP.(none)>
2012-05-24 11:21:12 -0400

    Corrected filename for includes Added h1 Added Form elements: Username, Password, Preferred Language, Notes, Accept Terms and Submit button

--- mtm1531/assignment-3/index.php
+++ mtm1531/assignment-3/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'includes/form-processor.php';
+require_once 'includes/registform-processor.php';
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -10,6 +10,7 @@ require_once 'includes/form-processor.php';
 </head>
 
 <body>
+	<h1>Registration Form</h1>
 	<form method= "post" action="index.php">
     	<div>
 			<label for="name">Name <?php if(isset($errors['name'])): ?>
@@ -23,10 +24,52 @@ require_once 'includes/form-processor.php';
             	<strong class="error">is required</strong>
             	<?php endif;?>
              </label>
-            <input type="email" id="email" name="email" value"<?php echo $email;?>">
+            <input type="email" id="email" name="email" required value"<?php echo $email;?>">
         </div>
-
-
+		<div>
+			<label for="username">User Name <?php if(isset($errors['username'])): ?>
+				<strong class="error">is required</strong>
+				<?php endif;?>
+			</label>
+			<input id="username" name="username" required value="<?php echo $username; ?>">
+        </div>
+		<div>
+			<label for="password">Password <?php if(isset($errors['password'])): ?>
+				<strong class="error">is required</strong>
+				<?php endif;?>
+			</label>
+			<input id="password" name="password" required value="<?php echo $password; ?>">
+        </div>
+		<div>
+        <fieldset>
+        	<legend>Preferred Language</legend>
+            
+            <input type= "radio" id="english" name="preferredlang" value="english">
+            <label for="english">English</label>
+            
+            <input type= "radio" id="french" name="preferredlang" value="french">
+            <label for="french">French</label>
+            
+            <input type= "radio" id="spanish" name="preferredlang" value="spanish">
+            <label for="spanish">Spanish</label>
+		</fieldset>
+        </div>
+		<div>
+        	<label for="notes">Notes
+            
+            </label>
+            <textarea id="notes" name="notes" value="<?php echo $notes;?>"></textarea>
+            <!--<p>5 to 100 characters long</p> -->
+        </div>
+		 <div>
+        	<input type="checkbox" id="acceptterms" name="acceptterms" required value="1">
+            <label for="acceptterms">Accept terms?<?php if(isset($errors['acceptterms'])): ?>
+            	<strong class="error">You must comply!</strong><?php endif;?>            	
+                </label>
+                      
+        </div>
+		<button type="submit">Send</button>
+	</form>
 
 
 </body>
