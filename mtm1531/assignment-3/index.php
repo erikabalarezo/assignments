b799e91157b56f4ae19efb596597a74c49d569e2
unknown <owner@owner-HP.(none)>
2012-05-24 23:13:10 -0400

    Corrected validation for preferred language. Used array for possible preferred languages.

--- mtm1531/assignment-3/index.php
+++ mtm1531/assignment-3/index.php
@@ -1,4 +1,13 @@
 <?php
+
+
+$possible_preferredlang = array(
+	'english' => 'English'
+	, 'french' => 'French'
+	, 'spanish' => 'Spanish'
+);
+
+
 require_once 'includes/registform-processor.php';
 ?>
 <!DOCTYPE HTML>
@@ -12,62 +21,59 @@ require_once 'includes/registform-processor.php';
 <body>
 	<h1>Registration Form</h1>
 	<form method= "post" action="index.php">
-    	<div>
+		<div>
 			<label for="name">Name <?php if(isset($errors['name'])): ?>
 				<strong class="error">is required</strong>
 				<?php endif;?>
 			</label>
 			<input type="text" id="name" name="name" required value="<?php echo $name; ?>">
-        </div>
+		</div>
 		<div>
-        	<label for="email">E-mail Address<?php if(isset($errors['email'])): ?>
-            	<strong class="error">is required</strong>
-            	<?php endif;?>
-             </label>
-            <input type="email" id="email" name="email" required value"<?php echo $email;?>">
-        </div>
+			<label for="email">E-mail Address<?php if(isset($errors['email'])): ?>
+				<strong class="error">is required</strong>
+				<?php endif;?>
+			</label>
+			<input type="email" id="email" name="email" required value"<?php echo $email;?>">
+		</div>
 		<div>
 			<label for="username">User Name <?php if(isset($errors['username'])): ?>
 				<strong class="error">is required</strong>
 				<?php endif;?>
 			</label>
 			<input type="text" id="username" name="username" required value="<?php echo $username; ?>">
-        </div>
+		</div>
 		<div>
 			<label for="password">Password <?php if(isset($errors['password'])): ?>
 				<strong class="error">is required</strong>
 				<?php endif;?>
 			</label>
 			<input type="password" id="password" name="password" required value="<?php echo $password; ?>">
-        </div>
+		</div>
 		<div>
-        <fieldset>
-        	<legend>Preferred Language</legend>
-            
-            <input type= "radio" id="english" name="preferredlang" value="english">
-            <label for="english">English</label>
-            
-            <input type= "radio" id="french" name="preferredlang" value="french">
-            <label for="french">French</label>
-            
-            <input type= "radio" id="spanish" name="preferredlang" value="spanish">
-            <label for="spanish">Spanish</label>
+		<fieldset>
+			<legend>Preferred Language</legend>
+				<?php if (isset($errors['preferredlang'])) : ?><p class="selectlangmessage"><strong>Select Preferred Language</strong></p><?php endif; ?>
+				<?php foreach ($possible_preferredlang as $key => $value) : ?>
+					<input type= "radio" id="<?php echo $key; ?>" name="preferredlang" 
+						value="<?php echo $key; ?>"<?php if ($key == $preferredlang) { echo ' checked'; } ?>>
+					<label for="<?php echo $key; ?>"><?php echo $value; ?></label>
+				<?php endforeach; ?>
 		</fieldset>
-        </div>
+		</div>
 		<div>
-        	<label for="notes">Notes
-            
-            </label>
-            <textarea id="notes" name="notes" value="<?php echo $notes;?>"></textarea>
+			<label for="notes">Notes
+
+			</label>
+			<textarea id="notes" name="notes" value="<?php echo $notes;?>"></textarea>
             <!--<p>5 to 100 characters long</p> -->
-        </div>
-		 <div>
-        	<input type="checkbox" id="acceptterms" name="acceptterms" required value="1">
-            <label for="acceptterms">Accept terms?<?php if(isset($errors['acceptterms'])): ?>
-            	<strong class="error">You must comply!</strong><?php endif;?>            	
-                </label>
-                      
-        </div>
+		</div>
+		<div>
+			<input type="checkbox" id="acceptterms" name="acceptterms" required value="1">
+			<label for="acceptterms">Accept terms?<?php if(isset($errors['acceptterms'])): ?>
+				<strong class="error">You must comply!</strong><?php endif;?>            	
+				</label>
+
+		</div>
 		<button type="submit">Send</button>
 	</form>
 
