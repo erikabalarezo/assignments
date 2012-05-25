5237b76d51c1da144be07a0cc36de5c3625dc578
unknown <owner@owner-HP.(none)>
2012-05-24 22:19:53 -0400

    Added validation for name, email, username, password, preferredlang.

--- mtm1531/assignment-3/includes/registform-processor.php
+++ mtm1531/assignment-3/includes/registform-processor.php
@@ -6,3 +6,23 @@ $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 $preferredlang =filter_input(INPUT_POST, 'preferredlang', FILTER_SANITIZE_STRING);
 $notes =filter_input(INPUT_POST, 'notes', FILTER_SANITIZE_STRING);
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if(empty($name)) {
+		$errors['name'] = true;
+	}
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = true;
+	}
+	if(empty($username)) {
+		$errors['username'] = true;
+	}
+	if(empty($password)) {
+		$errors['password'] = true;
+	}
+	if($preferredlang != 'english' || $preferredlang != 'french' || $preferredlang != 'spanish'){
+		$errors['preferredlang'] = true;
+	}
+	
+	
+}
