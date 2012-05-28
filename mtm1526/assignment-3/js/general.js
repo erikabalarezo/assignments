fe2b82c96ea6bff95870f9c73be1664c1adaa5c2
erikabalarezo <erikabalarezo@yahoo.com>
2012-05-28 15:56:32 -0400

    Updated variables

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,25 +1,42 @@
 $(document).ready(function(){
 	console.log('IN jQuery');
 	//closs platform use on
+	var property;
+	var myColor;
+	
 	$('#bordercolor').on('click', function(ev) {
-		$(this).toggleClass('changeBorder');
+		property = $('#bordercolor').val();
+		
+		//$(this).toggleClass('changeBorder');
 	// JavaScript Document
 	});
 	$('#backgroundcolor').on('click', function(ev) {
-		$(this).toggleClass('backgroundcolor');
+		property = $('#backgroundcolor').val();
+		
+		//$(this).toggleClass('backgroundcolor');
 	// JavaScript Document
+	});
+	$('#color').on('click', function(ev) {
+		property = $('#color').val();
+		
+		//$(this).toggleClass('backgroundcolor');
+	// JavaScript Document
+	});
+	
+	
+	bind(document.getElementById('color-box'), 'change', function (ev) {
+	//INside an event Listner
+	//this referers to the element this listener was bound to
+		myColor = this.value;
+		//we can put any htm inside
+		
 	});
 	
 	$('.todos').on('click', 'h1', function() {
 		$(this).toggleClass('done');
 		
 	});
-	bind(document.getElementById('color-box'), 'change', function (ev) {
-	//INside an event Listner
-	//this referers to the element this listener was bound to
-	myColor = this.value;
-	//we can put any htm inside
-	circle.innerHTML = '<strong>' + this.value + '</strong>';
-});
+	var circlenewcss = property': ' + myColor;
+	$('.myCircle').val('circlenewcss');
 
 });
