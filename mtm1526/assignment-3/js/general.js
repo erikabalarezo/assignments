e44a5bce32ae948c056a54d572c0a9346a5906fb
unknown <owner@owner-HP.(none)>
2012-05-29 11:39:46 -0400

    Cleaned code. Added variables property and ballcolor to store values selected in the form. To fix: Circle changes color but doesn't stay.

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -2,41 +2,31 @@ $(document).ready(function(){
 	console.log('IN jQuery');
 	//closs platform use on
 	var property;
-	var myColor;
+	var ballcolor;
+	var circle;
+	var stylecircle;
 	
-	$('#bordercolor').on('click', function(ev) {
-		property = $('#bordercolor').val();
-		
-		//$(this).toggleClass('changeBorder');
-	// JavaScript Document
+	$('#bordercolor').on('click', function() {
+		property = 'border-color';
+		console.log('BORDER COLOR CLICKED');
 	});
-	$('#backgroundcolor').on('click', function(ev) {
-		property = $('#backgroundcolor').val();
-		
-		//$(this).toggleClass('backgroundcolor');
-	// JavaScript Document
+	$('#backgroundcolor').on('click', function() {
+		property = 'background-color';
+		console.log('BACKGROUND COLOR CLIKED');
 	});
-	$('#color').on('click', function(ev) {
-		property = $('#color').val();
-		
-		//$(this).toggleClass('backgroundcolor');
-	// JavaScript Document
+	$('#color').on('click', function() {
+		console.log('COLOR FROM BOX SELECTION');
+		property = 'color';
 	});
-	
-	
-	bind(document.getElementById('color-box'), 'change', function (ev) {
-	//INside an event Listner
-	//this referers to the element this listener was bound to
-		myColor = this.value;
-		//we can put any htm inside
+	$('#color-box').on('change', function() {
+		console.log('WROTE A COLOR');
+		ballcolor = $('#color-box').val();
+		console.log(ballcolor);
+	});
+	$('#apply').on('click', function() {
+		console.log('clicked APPLY BUTTON');
+		$('.myCircle').css( property, ballcolor);
 		
 	});
 	
-	$('.todos').on('click', 'h1', function() {
-		$(this).toggleClass('done');
-		
-	});
-	var circlenewcss = property': ' + myColor;
-	$('.myCircle').val('circlenewcss');
-
 });
