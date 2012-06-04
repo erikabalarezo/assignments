1c4e51ad32f7cdf9bda1d7b93002f044a8e4a0dd
erikabalarezo <erikabalarezo@yahoo.com>
2012-06-04 14:16:10 -0400

    Added single.php

--- dinosaurs/index.php
+++ dinosaurs/index.php
@@ -1,17 +1,36 @@
-<!DOCTYPE HTML>
+<?php
+require_once 'includes/db.php';
+
+$sql =$db->query('
+	SELECT id, dino_name, loves_meat, in_jurassic_park
+	FROM dinosaurs
+	ORDER BY dino_name ASC
+');
+//debuggin only
+//var_dump($db->errorInfo());
+//use query string single.php?id to send us to other pages
+
+$results = $sql->fetchALL();
+//results is array
+//dino variables is for each iteration
+?><!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Dinosaurs</title>
+    <meta charset="utf-8">
+    <title>Dinosaurs</title>
 </head>
 
 <body>
-	<h2>Velociraptor</h2>
+	<?php foreach($results as $dino): ?>
+	<h2><a href="single.php?id=<?php echo $dino['id'];?>">
+	
+		<?php echo $dino['dino_name'];?></h2>
 	<dl>
     	<dt>Loves meat</dt>
-        <dd>1</dd>
+        <dd><?php echo $dino['loves_meat'];?></dd>
         <dt>In Jurassic Park</dt>
-         <dd>1</dd>
+         <dd><?php echo $dino['in_jurassic_park'];?></dd>
 	</dl>
+    <?php endforeach;?>
 </body>
 </html>
