913a00c17176f2ea0aa23e0056eceef24ad62433
erikabalarezo <erikabalarezo@yahoo.com>
2012-06-11 14:46:34 -0400

    Fixed error.

--- dinosaurs/index.php
+++ dinosaurs/index.php
@@ -23,6 +23,7 @@ $results = $sql->fetchALL();
 <body>
 
 	<a href="add.php">Add a Dinosaur</a>
+    
 	<?php foreach($results as $dino): ?>
 	<h2><a href="single.php?id=<?php echo $dino['id'];?>">
 	
