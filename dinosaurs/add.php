097989478ca8cc14c671345ca057ded6f1cbc244
erikabalarezo <erikabalarezo@yahoo.com>
2012-06-11 13:21:49 -0400

    Added validation code php.

--- dinosaurs/add.php
+++ dinosaurs/add.php
@@ -1,4 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+$errors = array();
+
+$dino_name = filter_input(INPUT_POST, 'dino_name', FILTER_SANITIZE_STRING);
+$loves_meat = filter_input(INPUT_POST, 'loves_meat', FILTER_SANITIZE_NUMBER_INT);
+$in_jurassic_park = (isset($_POST['in_jurassic_park'])) ? 1 :0;
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if(strlen($dino_name)< 1 || strlen($dino_name)> 256) {
+		$errors['dino_name']=true;
+	}
+	if(!in_array($loves_meat, array(0,1))) {
+		$errors['loves_meat']=true;
+	}
+	if(empty($errors)) {
+		//do DB stuff
+	}
+}
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -9,21 +28,39 @@
 	<h1>Add New Dinosaur</h1> 
     <form method="post" action="add.php">
     	<div>
-        	<label for="dino_name">Dinosaur Name</label>
-            <input id= "dino_name" name="dino_name" required>
+        	<label for="dino_name">
+            	Dinosaur Name
+            	<?php if (isset($errors['dino_name'])):?>
+                <strong class="error">is required</strong>
+            	<?php endif; ?>
+            </label>
+            <input id= "dino_name" name="dino_name" required value="<?php echo $dino_name;?>">
         </div>
         <fieldset>
-        	<legend>Relationship with meat</legend>
-            <input type="radio" id="love" name="loves_meat" value="1">
+        	<legend>Relationship with meat
+            	<?php if (isset($errors['loves_meat'])):?>
+            	 <strong class="error">is required</strong>
+            	<?php endif; ?>
+            </legend>
+            <input type="radio" id="love" name="loves_meat" value="1"
+            	<?php if($loves_meat == 1): ?>checked<?php endif;?> > 
+            
             <label for="love">Loves Meat</label>
-            <input type="radio" id="hate" name="loves_meat" value="0">
+            <input type="radio" id="hate" name="loves_meat" value="0"
+            	<?php if($loves_meat == 0): ?>checked<?php endif;?> > 
+            
             <label for="hate">Hates Meat</label>
         </fieldset>
         
         <div>
-        	<input type="checkbox" id="in_jurassic_park" name="in_jurassic_park">
+        	<input type="checkbox" id="in_jurassic_park" name="in_jurassic_park"
+            	<?php if($in_jurassic_park == 1): ?>checked<?php endif;?> > 
+                
            <label for="in_jurassic_park">In_jurassic_park?</label>
         </div>
+        <button type="submit">Add</button>
+        
+        </form>
     
 </body>
 </html>
